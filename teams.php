e82f6e16d51b013b06865a88f38843e7e214be1b
ImaniMuya <imani.muya@gmail.com>
2020-10-11 18:35:13 -0500

    admin delete teams handle session end $http keep status past event marked use php time for db

--- teams.php
+++ teams.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: http://localhost:8080'); //TODO: make constants file
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: GET, DELETE");
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 header('Access-Control-Allow-Credentials: true');
 
@@ -36,6 +36,20 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
   }
 
   die_JSON($teams);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+  $id = $_GET["id"];
+  // $sql = $conn->prepare("DELETE FROM people WHERE team_id = :id");
+  // $sql->bindValue(':id', $id);
+  // if (!$sql->execute()) die_error("Failed while removing people.");
+
+  $sql = $conn->prepare("DELETE FROM teams WHERE team_id = :id");
+  $sql->bindValue(':id', $id);
+  if (!$sql->execute()) die_error("Failed while removing team.");
+  
+  http_response_code(200);
+  die("Removed team with ID $id");
 }
 
 
